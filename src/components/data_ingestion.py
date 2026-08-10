ce41ea198dd95a8b4469e3e14f19fd1bc2a67676
bensbasil <bensdbasil@gmail.com>
2026-08-10 13:02:11 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artificats',"train.csv")
-    train_data_path: str=os.path.join('artificats',"test.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
@@ -21,10 +21,10 @@ class DataIngestion:
     def initiate_data_ingestion(self):
          logging.info("Entered the data ingestion method or component")
          try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('src/notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
          
-            os.makdirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("Train Test Split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
@@ -32,7 +32,18 @@ class DataIngestion:
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
+            logging.info("Ingestion of the data is completed")
+            return(
 
-            train
-         except:
-            pass
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+
+            )
+            
+         except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+    
