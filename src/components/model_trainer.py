f1971b15ae65917c098665b8e0c15f17b9922bdf
bensbasil <bensdbasil@gmail.com>
2026-08-21 10:27:50 +0530

    Hyper Parameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,17 +39,58 @@ class ModelTrainer:
                 test_array[:,:-1],
                 test_array[:,-1]
             )
-            models={
+            models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbhors Classifier": KNeighborsRegressor(),
-                "XBGClassifier": XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "KNeighbors Regressor": KNeighborsRegressor(),
+                "XGB Regressor": XGBRegressor(),
+                "CatBoost Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            model_report:dict=evaluate_models(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,models=models)
+
+            param = {
+                    "Random Forest": {
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                    },
+
+                    "Decision Tree": {
+                        "criterion": [
+                            "squared_error",
+                            "absolute_error",
+                            "poisson"
+                        ]
+                    },
+
+                    "Gradient Boosting": {
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                    },
+
+                    "Linear Regression": {},
+
+                    "KNeighbors Regressor": {},
+
+                    "XGB Regressor": {
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                    },
+
+                    "CatBoost Regressor": {
+                        "depth": [6, 8, 10],
+                        "learning_rate": [0.01, 0.05, 0.1],
+                        "iterations": [30, 50, 100]
+                    },
+
+                    "AdaBoost Regressor": {
+                        "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                    }
+                }
+
+            model_report:dict=evaluate_models(X_train=x_train,y_train=y_train,X_test=x_test,y_test=y_test,models=models,param=param)
 
             ## To get best model score from dict
             best_model_name = max(model_report, key=model_report.get)
